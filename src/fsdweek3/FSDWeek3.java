28714b0f2f6de76d83aeff01c3df597a0937ca9f
QadriAtSheridan <qadrii@sheridancollege.ca>
2024-01-26 15:58:00 -0500

    added the value of cinema to be A24

---
+++ src/fsdweek3/FSDWeek3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fsdweek3;
+
+/**
+ *
+ * @author conne
+ */
+public class FSDWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 24;
+        String cinema = "A24";
+    }
+    
+}
